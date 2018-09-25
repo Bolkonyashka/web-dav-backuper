4beb311bdd80355efc941e0859114351e9dc7c2d
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2018-09-25 22:11:53 +0500

    v 0.5

--- backuper.py
+++ backuper.py
@@ -13,34 +13,39 @@ class Settings:
         self.exists_directories = ''
         self.files_hash = ''
         self.backup_interval = ''
-        self.cleaning_interval = ''
-        self.last_backup = ''
-        self.last_cleaning = ''
+        self.backups_to_cleaning = ''
         self.backup_catalog = ''
-        self.backup_in_action = False
-        self.console_in_action = False
+        self.backup_mode = False
+        self.console_mode = False
+        self.lock = Lock()
 
     def read_settings(self):
-        with open("settings.json", "r") as file:
-            settings_json = file.read()
-            self.settings_dict = json.loads(settings_json)
-            file.close()
+        with self.lock:
+            with open("settings.json", "r") as file:
+                settings_json = file.read()
+                self.settings_dict = json.loads(settings_json)
+                file.close()
         self.exists_directories = self.settings_dict["existsDirectories"]
         self.files_hash = self.settings_dict["filesHash"]
         self.backup_interval = self.settings_dict["backupInterval"]
-        self.cleaning_interval = self.settings_dict["cleaningInterval"]
-        self.last_backup = self.settings_dict["lastBackup"]
-        self.last_cleaning = self.settings_dict["lastCleaning"]
+        self.backups_to_cleaning = self.settings_dict["backupsCountToCleaning"]
         self.backup_catalog = os.path.normpath(self.settings_dict["catalog"])
 
     def write_settings(self):
-        with open("settings.json", "w") as file:
-            file.write(json.dumps(self.settings_dict))
-            file.close()
+        with self.lock:
+            with open("settings.json", "w") as file:
+                file.write(json.dumps(self.settings_dict))
+                file.close()
 
     def set_backup_interval(self, inter):
         self.settings_dict["backupInterval"] = inter
         self.backup_interval = inter
+
+    def conf_cleaning(self):
+        self.exists_directories = []
+        self.settings_dict["existsDirectories"] = []
+        self.files_hash = []
+        self.settings_dict["filesHash"] = []
 
 
 class Backuper(Thread):
@@ -66,17 +71,23 @@ class Backuper(Thread):
         self.get_started()
 
     def get_started(self):
+        self.settings.backup_mode = True
+        self.cleaning()
+        self.settings.backup_mode = False
+        counter = 0
         while True:
-            time.sleep(self.settings.backup_interval)
-            while self.settings.console_in_action:  # Ожидание завершения действий с консолью
+            while self.settings.console_mode:  # Ожидание завершения действий с консолью
                 pass
-            lock = Lock()
-            self.settings.backup_in_action = True  # Заглушка для консоли
-            with lock:
-                self.settings.read_settings()
-                self.do_backup()
-                self.settings.write_settings()
-            self.settings.backup_in_action = False
+            self.settings.backup_mode = True  # Заглушка для консоли
+            if counter == self.settings.backups_to_cleaning:
+                counter = 0
+                self.cleaning()
+            self.settings.read_settings()
+            self.do_backup()
+            self.settings.write_settings()
+            self.settings.backup_mode = False
+            counter += 1
+            time.sleep(self.settings.backup_interval)
 
     def get_list_for_backup(self, backup_path):
         backup_list = []
@@ -100,6 +111,23 @@ class Backuper(Thread):
         self.settings.files_hash.append({"filePath": file_path, "hash": file_hash})
         return True
 
+    def check_cloud_dirs(self, cloud_path):
+        cloud_path_splitted = cloud_path.split('/')
+        counter = 1  # 0 - пустая строка, 1 - каталог бекапера
+        cloud_path_part = ''
+        while counter < len(cloud_path_splitted) - 1:
+            cloud_path_part = cloud_path_part + '/' + cloud_path_splitted[counter]
+            if cloud_path_part not in self.settings.exists_directories:  # проверка существования директории в облаке
+                self.webdav_client.mkdir(cloud_path_part)
+                self.settings.exists_directories.append(cloud_path_part)
+            counter += 1
+
+    def form_cloud_path(self, file_path, dir_name):
+        file_path = file_path.replace('\\', '/')
+        cloud_path = self.settings.cloud_catalog + '/' + dir_name + '/' + file_path
+        return cloud_path
+
+
     def do_backup(self):
         print("Backup started")
         list_for_backup = self.get_list_for_backup(self.settings.backup_catalog)
@@ -108,29 +136,27 @@ class Backuper(Thread):
         for file_path in list_for_backup:
             full_file_path = os.path.join(self.settings.backup_catalog, file_path)
             if self.check_hash_list_and_update(full_file_path):
-                file_path_splitted = file_path.split('\\')
-                filename = file_path_splitted[len(file_path_splitted) - 1]
-                file_path_splitted.insert(0, dir_name)  # всегда вставляем имя директории
-                cloud_path = self.settings.cloud_catalog
-                counter = 0
-                while counter < len(file_path_splitted) - 1:
-                    cloud_path = cloud_path + '/' + file_path_splitted[counter]
-                    if cloud_path not in self.settings.exists_directories:  # проверка существования директории в облаке
-                        self.webdav_client.mkdir(cloud_path)
-                        self.settings.exists_directories.append(cloud_path)
-                    counter += 1
-                cloud_path += '/' + filename
+                cloud_path = self.form_cloud_path(file_path, dir_name)
+                self.check_cloud_dirs(cloud_path)
                 print("* backuping " + cloud_path)
                 self.webdav_client.upload(full_file_path, cloud_path)
                 print("*** done")
         print("Backup completed")
+
+    def cleaning(self):
+        print("Cleaning in action")
+        self.webdav_client.delete(self.settings.cloud_catalog)
+        self.settings.conf_cleaning()
+        self.settings.write_settings()
+        print("Cleaning completed")
 
 
 class ConsoleInterface(Thread):
     def __init__(self, sett):
         Thread.__init__(self)
         self.settings = sett
-        self.command_dict = {"chinterval": self.change_backup_interval}
+        self.command_dict = {"conf": self.change_console_mode, "chinterval": self.change_backup_interval,
+                             "exit": self.change_console_mode}
 
     def run(self):
         self.input_action()
@@ -150,17 +176,23 @@ class ConsoleInterface(Thread):
                 print("** The new interval is saved. It will take effect after the next backup.")
                 break
 
+    def change_console_mode(self):
+        self.settings.console_mode = not self.settings.console_mode  # Заглушка для бекапера
+        if self.settings.console_mode:
+            print("Configure mode is activated")
+        else:
+            print("Configure mode is deactivated")
+
     def input_action(self):
         while True:
             command = input()
-            self.settings.console_in_action = True  # Заглушка для бекапера
-            if not self.settings.backup_in_action:
+            if not self.settings.backup_mode:
                 if command not in self.command_dict.keys():
                     print("Unknown command")
+                elif not self.settings.console_mode and command != "conf":
+                    print("Enter the 'conf' command to change the configuration")
                 else:
-                    lock = Lock()
-                    with lock:
-                        self.command_dict[command]()
+                    self.command_dict[command]()
             else:
                 print("(Blocked) Backup in action. Wait for the backup to complete and enter the command again!")
             self.settings.console_in_action = False
