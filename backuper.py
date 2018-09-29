8f4f84de6c6a5e8e5b30b6a308b06eb5b2b99613
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2018-09-29 21:08:57 +0500

    v 1.0

--- backuper.py
+++ backuper.py
@@ -1,5 +1,5 @@
 import os
-import easywebdav
+import easywebdavfixed
 import json
 import hashlib
 import time
@@ -14,7 +14,7 @@ class Settings:
         self.files_hash = ''
         self.backup_interval = ''
         self.backups_to_cleaning = ''
-        self.backup_catalog = ''
+        self.backup_catalogs = []
         self.backup_mode = False
         self.console_mode = False
         self.lock = Lock()
@@ -29,7 +29,7 @@ class Settings:
         self.files_hash = self.settings_dict["filesHash"]
         self.backup_interval = self.settings_dict["backupInterval"]
         self.backups_to_cleaning = self.settings_dict["backupsCountToCleaning"]
-        self.backup_catalog = os.path.normpath(self.settings_dict["catalog"])
+        self.backup_catalogs = self.settings_dict["catalogs"]
 
     def write_settings(self):
         with self.lock:
@@ -40,6 +40,20 @@ class Settings:
     def set_backup_interval(self, inter):
         self.settings_dict["backupInterval"] = inter
         self.backup_interval = inter
+
+    def set_backups_to_cleaning(self, count):
+        self.settings_dict["backupsCountToCleaning"] = count
+        self.backups_to_cleaning = count
+
+    def set_auth(self, login, passw):
+        self.settings_dict["login"] = login
+        self.settings_dict["pass"] = passw
+
+    def add_backup_catalog(self, new_path):
+        self.backup_catalogs.append(new_path)
+
+    def del_backup_catalog(self, del_path):
+        self.backup_catalogs.remove(del_path)
 
     def conf_cleaning(self):
         self.exists_directories = []
@@ -52,10 +66,11 @@ class Backuper(Thread):
     def __init__(self, sett):
         Thread.__init__(self)
         self.settings = sett
-        self.webdav_client = easywebdav.connect(protocol='https', host=self.settings.settings_dict["host"],
+        self.webdav_client = easywebdavfixed.Client(protocol='https', host=self.settings.settings_dict["host"],
                                                 port=self.settings.settings_dict["port"],
                                                 username=self.settings.settings_dict["login"],
                                                 password=self.settings.settings_dict["pass"])
+        self.main_catalog = ""
 
     def get_hash_md5(self, filename):
         with open(filename, 'rb') as f:
@@ -79,7 +94,7 @@ class Backuper(Thread):
             while self.settings.console_mode:  # Ожидание завершения действий с консолью
                 pass
             self.settings.backup_mode = True  # Заглушка для консоли
-            if counter == self.settings.backups_to_cleaning:
+            if counter >= self.settings.backups_to_cleaning:
                 counter = 0
                 self.cleaning()
             self.settings.read_settings()
@@ -94,7 +109,7 @@ class Backuper(Thread):
         for file in os.listdir(backup_path):
             path = os.path.join(backup_path, file)
             if not os.path.isdir(path):
-                backup_list.append(path.replace(self.settings.backup_catalog + '\\', ''))
+                backup_list.append(path.replace(self.main_catalog + '\\', ''))
             else:
                 backup_list += self.get_list_for_backup(path)
         return backup_list
@@ -130,17 +145,23 @@ class Backuper(Thread):
 
     def do_backup(self):
         print("Backup started")
-        list_for_backup = self.get_list_for_backup(self.settings.backup_catalog)
-        dir_name = self.settings.backup_catalog.split('\\')
-        dir_name = dir_name[len(dir_name) - 1]  # имя директории, которую бекапим
-        for file_path in list_for_backup:
-            full_file_path = os.path.join(self.settings.backup_catalog, file_path)
-            if self.check_hash_list_and_update(full_file_path):
-                cloud_path = self.form_cloud_path(file_path, dir_name)
-                self.check_cloud_dirs(cloud_path)
-                print("* backuping " + cloud_path)
-                self.webdav_client.upload(full_file_path, cloud_path)
-                print("*** done")
+        for catalog in self.settings.backup_catalogs:
+            catalog = os.path.normpath(catalog)
+            if not os.path.isdir(catalog):
+                self.settings.backup_catalogs.remove(catalog)
+                continue
+            self.main_catalog = catalog
+            list_for_backup = self.get_list_for_backup(catalog)
+            dir_name = catalog.split('\\')
+            dir_name = dir_name[len(dir_name) - 1]  # имя директории, которую бекапим
+            for file_path in list_for_backup:
+                full_file_path = os.path.join(catalog, file_path)
+                if self.check_hash_list_and_update(full_file_path):
+                    cloud_path = self.form_cloud_path(file_path, dir_name)
+                    self.check_cloud_dirs(cloud_path)
+                    print("* backuping " + cloud_path)
+                    self.webdav_client.upload(full_file_path, cloud_path)
+                    print("*** done")
         print("Backup completed")
 
     def cleaning(self):
@@ -156,10 +177,63 @@ class ConsoleInterface(Thread):
         Thread.__init__(self)
         self.settings = sett
         self.command_dict = {"conf": self.change_console_mode, "chinterval": self.change_backup_interval,
-                             "exit": self.change_console_mode}
+                             "exit": self.change_console_mode, "chbtc": self.change_backups_to_cleaning,
+                             "chauth": self.change_auth, "adddir": self.add_directory, "deldir": self.del_directory}
 
     def run(self):
         self.input_action()
+
+    def del_directory(self):
+        print("* Enter the directory path to remove:")
+        del_path = input()
+        if del_path not in self.settings.backup_catalogs:
+            print("** This path is not exist in backup catalogs list.")
+        else:
+            self.settings.del_backup_catalog(del_path)
+            self.settings.write_settings()
+            print("*** The directory path is removed.")
+
+    def add_directory(self):
+        print("* Enter the new directory for backup path:")
+        new_path = os.path.normpath(input())
+        if not os.path.isdir(new_path):
+            print("** Path is incorrect or it is not a directory!")
+        elif new_path in self.settings.backup_catalogs:
+            print("** This path already exists in list for backup.")
+        else:
+            self.settings.add_backup_catalog(new_path)
+            self.settings.write_settings()
+            print("*** The new directory for backup path is saved.")
+
+
+    def change_auth(self):
+        new_login = ""
+        new_pass = ""
+        while len(new_login) < 3:
+            print("* Enter the login (min a 3 characters):")
+            new_login = input()
+        while len(new_pass) < 3:
+            print("* Enter the password (min a 3 characters):")
+            new_pass = input()
+        self.settings.set_auth(new_login, new_pass)
+        self.settings.write_settings()
+        print("*** The new auth data is saved.")
+
+
+    def change_backups_to_cleaning(self):
+        print("* Enter the number of backups after which the disk will be cleaned up:")
+        while True:
+            new_count = input()
+            try:
+                new_count = int(new_count)
+            except ValueError:
+                print("** Not a number")
+                continue
+            else:
+                self.settings.backups_to_cleaning = new_count
+                self.settings.write_settings()
+                print("*** The new value is saved.")
+                break
 
     def change_backup_interval(self):
         print("* Enter the new interval (in sec):")
@@ -173,7 +247,7 @@ class ConsoleInterface(Thread):
             else:
                 self.settings.set_backup_interval(new_interval)
                 self.settings.write_settings()
-                print("** The new interval is saved. It will take effect after the next backup.")
+                print("*** The new interval is saved. It will take effect after the next backup.")
                 break
 
     def change_console_mode(self):
